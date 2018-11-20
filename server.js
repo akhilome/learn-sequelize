4ce5b1b69e4dcea5932499461c07148bb930f771
Kizito Akhilome <hovkard@gmail.com>
2018-11-20 17:00:12 +0100

    create a database connection

--- server.js
+++ server.js
@@ -4,4 +4,16 @@ const Sequelize = require('sequelize');
 const app = express();
 const port = 3000;
 
+const connection = new Sequelize('db', 'user', 'pass', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: 'db.sqlite',
+  operatorsAliases: false
+});
+
+connection
+  .authenticate()
+  .then(() => console.log('Connection to db successful'))
+  .catch(err => console.error('Unable to establish connection: ', err));
+
 app.listen(port, () => console.log('app started!'));
