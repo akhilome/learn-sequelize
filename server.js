9eea27eabf83d9136326d703ef937bca750c9466
Kizito Akhilome <hovkard@gmail.com>
2018-11-20 18:51:10 +0100

    learn more basic stuff about using sequelize ...
    - what models are & how to create them
    - how to sync with a db & how to insert data
    - common SQL data types & how they can be specified on a model
    - what primary keys are & their importance

--- server.js
+++ server.js
@@ -11,8 +11,33 @@ const connection = new Sequelize('db', 'user', 'pass', {
   operatorsAliases: false
 });
 
+//=> This is a Model. 👇🏾👇🏾
+const User = connection.define('User', {
+  // Models are created using the 'define' method 
+  // on the connection 👆🏾👆🏾
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
+  },
+  name: Sequelize.STRING,
+  bio: Sequelize.TEXT
+  // 👆🏾👆🏾 the data types for each column/attribute 
+  // is specified as Sequelize.[data_type] above
+});
+
 connection
-  .authenticate()
+  .sync({
+    logging: console.log,
+    force: true
+  })
+  .then(() => {
+    // 👇🏾👇🏾 Inserting new data into db
+    User.create({
+      name: 'Kay',
+      bio: 'Learning Sequelize'
+    });
+  })
   .then(() => console.log('Connection to db successful'))
   .catch(err => console.error('Unable to establish connection: ', err));
 
