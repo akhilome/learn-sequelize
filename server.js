3fb7c87de9661d619d25510729ce040c43100a0b
Kizito Akhilome <hovkard@gmail.com>
2018-11-20 20:18:16 +0100

    add sample data to db using bulkCreate
    - also, purge db of existing junk data manually

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Sequelize = require('sequelize');
+const _USERS = require('./users.json');
+
 
 const app = express();
 const port = 3000;
@@ -11,71 +13,23 @@ const connection = new Sequelize('db', 'user', 'pass', {
   operatorsAliases: false
 });
 
-//=> This is a Model. 👇🏾👇🏾
 const User = connection.define('User', {
-  // Models are created using the 'define' method 
-  // on the connection 👆🏾👆🏾
-  uuid: {
-    type: Sequelize.UUID,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4
-  },
-  first: Sequelize.STRING,
-  last: Sequelize.STRING,
-  full_name: {
+  name: Sequelize.STRING,
+  email: {
     type: Sequelize.STRING,
-    // 👇🏾👇🏾 Do some validation before sending data to db
     validate: {
-      len: {
-        args: [7],
-        msg: 'Error: Full Name must be longer than 7 chars'
-        // 👆🏾👆🏾 Custom error message to be sent if validation fails
-      }
-    },
-    allowNull: false // 👈🏾👈🏾 full_name cannot be null!
-  },
-  bio: Sequelize.TEXT
-  // 👆🏾👆🏾 the data types for each column/attribute 
-  // is specified as Sequelize.[data_type] above
-}, {
-  // 👇🏾👇🏾 Look, ma. Hooks! ↩️
-  hooks: {
-    beforeValidate: (user) => {
-      user.full_name = `${user.first} ${user.last}`
+      isEmail: true
     }
-    // 👆🏾👆🏾 This runs before validation is done on the 
-    // provided data.
+  },
+  password: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlphanumeric: true
+    }
   }
 });
 
-// 👇🏾👇🏾 Trigger a validation error 😠
-app.get('/fail', (req, res) => {
-  User.create({
-    first: 'Ki',
-    last: 'Zi',
-    bio: 'Learning Sequelize kinda well 🤔'
-  }).then(user => {
-    res.json(user);
-  }).catch(error => {
-    console.error(error);
-    res.status(404).json(error);
-  });
-});
-
-// 👇🏾👇🏾 Trigger a successful insertion 🎊🕺🏾
-app.get('/pass', (req, res) => {
-  User.create({
-    first: 'Kay',
-    last: 'Erons',
-    bio: 'Learning Sequelize very well 😀'
-  }).then(user => {
-    res.json(user);
-  }).catch(error => {
-    console.error(error);
-    res.status(404).json(error);
-  });
-});
-
+// For personal use only 👀
 app.get('/users', (req, res) => {
   User.findAll().then(users => {
     res.json(users);
@@ -84,29 +38,14 @@ app.get('/users', (req, res) => {
 
 connection
   .sync({
-    logging: console.log,
-    force: true // 👈🏾👈🏾 purges db each time connection is initiated
+    // logging: console.log
   })
   .then(() => {
-    // 👇🏾👇🏾 Inserting new data into db
-    // User.create({
-    //   first: 'Kay',
-    //   last: 'Erons',
-    //   bio: 'Learning Sequelize'
-    // });
+    User.bulkCreate(_USERS)
+      .then(users => console.log('success adding users'))
+      .catch(err => console.error(err))
   })
   .then(() => console.log('Connection to db successful'))
   .catch(err => console.error('Unable to establish connection: ', err));
 
 app.listen(port, () => console.log('app started!'));
-
-
-/* 📓📓📓 Notes: 
-  => Sequelize Hooks: do somethings before/after certain events,
-    some examples ...
-    -> beforeValidate: () => { }
-    -> afterValidate: () => { }
-    -> beforeCreate: () => { }
-    -> afterCreate: () => { }
-    -> want more 🤔? see the docs! 😜
-*/
